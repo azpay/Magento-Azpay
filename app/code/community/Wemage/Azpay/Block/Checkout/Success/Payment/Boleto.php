b0d9e25d93876fab3d92c2f08f7cd1f20d802424
Lucas Palencia <lucaspalencia@azclick.com.br>
2016-08-11 11:04:37 -0300

    update boleto button success page

--- app/code/community/Wemage/Azpay/Block/Checkout/Success/Payment/Boleto.php
+++ app/code/community/Wemage/Azpay/Block/Checkout/Success/Payment/Boleto.php
@@ -4,7 +4,7 @@ class Wemage_Azpay_Block_Checkout_Success_Payment_Boleto extends Wemage_Azpay_Bl
 
     public function getBoletoUrl() {
 
-      $order = Mage::getModel('sales/order')->load(Mage::getSingleton('checkout/session')->getLastOrderId());
+      $order = $this->getOrder();
       $resource = Mage::getSingleton('core/resource');
     	$readConnection = $resource->getConnection('core_read');
     	$query = 'SELECT additional_data FROM sales_flat_order_payment WHERE entity_id = '.$order->getId().' LIMIT 1';
